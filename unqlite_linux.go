d2ddd80b9e67c06cdc6c8bd24cd6a6eb9a1ecf08
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-03-20 00:40:29 +0100

    Update CGO

--- unqlite_linux.go
+++ unqlite_linux.go
@@ -2,6 +2,6 @@
 
 package unqlitego
 
-// #cgo LDFLAGS: -lm -lpthread
-// #cgo CFLAGS: -DUNQLITE_ENABLE_THREADS=1 -Wno-unused-but-set-variable -DJX9_ENABLE_MATH_FUNC -DUNQLITE_ENABLE_JX9_HASH_IO
+// #cgo linux LDFLAGS: -lm -lpthread
+// #cgo linux CFLAGS: -DUNQLITE_ENABLE_THREADS -DJX9_ENABLE_MATH_FUNC -DUNQLITE_ENABLE_JX9_HASH_IO -Wno-unused-but-set-variable
 import "C"
