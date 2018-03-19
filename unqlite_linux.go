4ac18a964121f04bb2f678b4163119a79fe4370f
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-03-20 00:29:42 +0100

    Update CGO Directives

--- unqlite_linux.go
+++ unqlite_linux.go
@@ -1,3 +1,7 @@
-// +build linux,cgo CFLAGS: -DUNQLITE_ENABLE_THREADS=1 -DJX9_ENABLE_MATH_FUNC=1 -DUNQLITE_ENABLE_JX9_HASH_IO=1 -Wno-unused-but-set-variable -lpthread -lm
+// +build linux,cgo
 
 package unqlitego
+
+// #cgo LDFLAGS: -lm -lpthread
+// #cgo CFLAGS: -DUNQLITE_ENABLE_THREADS=1 -Wno-unused-but-set-variable -DJX9_ENABLE_MATH_FUNC -DUNQLITE_ENABLE_JX9_HASH_IO
+import "C"
